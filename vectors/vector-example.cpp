5ddf2163e0a924fa2f3f3f674de22e639a5e75d3
Sever Hussein <sever.hussein@gmail.com>
2021-03-17 23:06:22 -0400

    finish vector examples and create a helpful.txt file which contains useful c++ information

--- vectors/vector-example.cpp
+++ vectors/vector-example.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    auto printLambda = [](const auto& elem) { std::cout << elem << " "; };
+
+    //create empty vector then add 3 elements
+    std::vector<int> vec1;
+    vec1.push_back(1);
+    vec1.push_back(2);
+    vec1.push_back(3);
+    std::for_each(vec1.begin(),vec1.end(), printLambda);
+    std::cout << std::endl;
+
+    //vector with 3 elements
+    std::vector<int> vec2{4,5,6};
+    std::for_each(vec2.begin(),vec2.end(),printLambda);
+    std::cout << std::endl;
+
+    //vector with 3 elements
+    std::vector<int> vec3 = {7,8,9};
+    std::for_each(vec3.begin(),vec3.end(),printLambda);
+    std::cout << std::endl;
+
+    //vector with size n
+    int n = 3;
+    std::vector<int> vec4(n);
+    std::cout << "vec4 size=" <<vec4.size() << std::endl;
+    vec4[0]=10;
+    vec4[1]=11;
+    vec4[2]=12;
+    std::for_each(vec4.begin(),vec4.end(),printLambda);
+    std::cout << std::endl;
+
     return EXIT_SUCCESS;
 }
